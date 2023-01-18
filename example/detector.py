3a0cd3d2146453af83461c13077f913cd677860d
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2023-01-18 11:57:45 +0000

    update example detector

--- example/detector.py
+++ example/detector.py
@@ -1,7 +1,8 @@
 from scipy import stats
 
 #ksTest detector
-def detector(trainSet, liveSet):
+def detector(trainSet, liveSet, parameters):
     firstFeature = trainSet.axes[1][0]
     pValue = stats.ks_2samp(trainSet[firstFeature].to_list(), liveSet[firstFeature].to_list())[1]
-    return int(pValue < 0.05), pValue
+    threshold = float (parameters.get("pValue", 0.05))
+    return int(pValue < threshold), pValue
